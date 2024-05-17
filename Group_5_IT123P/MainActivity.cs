f496df88b92175bc61795d1bc3851172ef4a9d2b
JurisAlson <jurisalson@gmail.com>
2024-05-17 10:55:38 +0800

    s

--- Group_5_IT123P/MainActivity.cs
+++ Group_5_IT123P/MainActivity.cs
@@ -1,26 +1,25 @@
 ﻿using Android.App;
 using Android.OS;
 using Android.Runtime;
+using Android.Widget;
 using AndroidX.AppCompat.App;
 
 namespace Group_5_IT123P
 {
-    [Activity(Label = "@string/app_name", Theme = "@style/AppTheme", MainLauncher = true)]
+    [Activity(Label = "Color Game", Theme = "@style/AppTheme", MainLauncher = true)]
     public class MainActivity : AppCompatActivity
     {
         protected override void OnCreate(Bundle savedInstanceState)
         {
+            Button ROLL, STOP;
+
             base.OnCreate(savedInstanceState);
             Xamarin.Essentials.Platform.Init(this, savedInstanceState);
-            // Set our view from the "main" layout resource
             SetContentView(Resource.Layout.activity_main);
         }
         public override void OnRequestPermissionsResult(int requestCode, string[] permissions, [GeneratedEnum] Android.Content.PM.Permission[] grantResults)
         {
             Xamarin.Essentials.Platform.OnRequestPermissionsResult(requestCode, permissions, grantResults);
-
-
-         
             base.OnRequestPermissionsResult(requestCode, permissions, grantResults);
         }
     }
